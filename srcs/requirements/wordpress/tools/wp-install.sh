2e30e443623a2b6e03b6d05be87459d96d28e9ca
Ayman-SIDQI <sidqi.ayman.96@gmail.com>
2024-04-25 15:35:09 -0400

    added chown statement

--- srcs/requirements/wordpress/tools/wp-install.sh
+++ srcs/requirements/wordpress/tools/wp-install.sh
@@ -1,5 +1,6 @@
 if [ ! -f "wp-config.php" ]; then
 	wp core download
+	chmod -R 0777 /var/www/wp-content
 	wp config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=mariadb:3306 --allow-root
 	wp core install --url="localhost" --title="Inception" --admin_user="$MARIADB_USER" \
 	--admin_password="$MARIADB_PASSWORD" --admin_email="example@gmail.com" --skip-email
