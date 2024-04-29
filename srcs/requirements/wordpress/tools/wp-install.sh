582ed6edeed290fceda36ef1f03b6c057e86d2b1
Ayman-SIDQI <sidqi.ayman.96@gmail.com>
2024-04-29 17:43:53 -0400

    redis almost done need to fix the chown problemo

--- srcs/requirements/wordpress/tools/wp-install.sh
+++ srcs/requirements/wordpress/tools/wp-install.sh
@@ -5,6 +5,9 @@ if [ ! -f "wp-config.php" ]; then
 	--admin_password="$MARIADB_PASSWORD" --admin_email="sidqi.ayman.96@gmail.com" --skip-email
 	wp user create $MARIADB_ADMIN_USER ana@gmail.com --role=author --user_pass=$MARIADB_ADMIN_PWD --allow-root
 	wp theme install twentysixteen --activate
+	wp plugin install redis-cache 
+	wp plugin activate redis-cache 
+	echo "define( 'WP_REDIS_HOST', 'redis' ); define( 'WP_REDIS_PORT', 6379 ); define('WP_CACHE', true);" >> /var/www/html/wp-config.php
 fi
         chmod -R 0777 /var/www/
         chown -R www-data:www-data /var/www/
